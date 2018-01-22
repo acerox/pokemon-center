ae1a7edd3b9575aa2eba5cac3073de270783aa88
acerox <agustincc4@gmail.com>
2018-01-22 13:53:25 +0100

    Starting with design mockup

--- pokemon-center/DashboardWindow.xaml.cs
+++ pokemon-center/DashboardWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace pokemon_center
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para DashboardWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DashboardWindow : Window
     {
-        public MainWindow()
+        public DashboardWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
