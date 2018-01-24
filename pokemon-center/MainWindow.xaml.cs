38b22d902c17555805759a0c1096acb3ded76378
Fragmo <marcferper@gmail.com>
2018-01-24 18:12:31 +0100

    hecho inicio de sesion

--- pokemon-center/MainWindow.xaml.cs
+++ pokemon-center/MainWindow.xaml.cs
@@ -12,6 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//añadimos los import de mysql
+
+using System.Data.Odbc;
+using MySql.Data.Types;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
 
 namespace pokemon_center
 {
@@ -20,14 +28,73 @@ namespace pokemon_center
     /// </summary>
     public partial class MainWindow : Window
     {
+            
+        // este es el ejemplo que conectara con una bbdd de mysql
+        // necesita 5 parametros:
+        //Server: la ip o nombre del servidor 
+        // Database : nombre de la base de datos 
+        //Uid: usuario (OJO: no se puede dejar n blanco)
+        //Pwd: clave del usuario si la tuviera
+        // Port: default: 3306
+
+        //guarda los parametros de la conexion 
+        private string connStr;
+
+        //variable que maneja la conexion
+        private MySqlConnection conexion;
+
+        //variable para almacenar la consulta a la bbdd
+        private String sentenciaSQL;
+
+        //variable que sirve para crear la conexion
+        private static MySqlCommand comando;
+
+        //guarda el resultado de la consulta
+        private MySqlDataReader resultado;
+
+        //pone el resultado de la bbdd en esta variable
+        private DataTable datos = new DataTable();
+
+        private int contadorFila = 0;
+        private int numeroFilas = 0;
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
-        }
+            conexion = new MySqlConnection("Server = 127.0.0.1; Database = pokemon-center; Uid = root; Pwd =; Port = 3306");
+            try
+            {
+                conexion.Open();
+                MessageBox.Show("Conexion a la bbdd realizada");
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+            }
+            catch (Exception ex){
+                MessageBox.Show("No se pudo conectar a la bbdd" + ex.ToString());
+            }
+            
+        }
+        
+
+        private void botonAcceder_Click(object sender, RoutedEventArgs e)
         {
 
+            //  PARA EL NOMBRE DE USUARIO
+            comando = new MySqlCommand("select nurse.username, nurse.password from nurse where username ='" + textoUsuario.Text+ "' and password ='" + textoContraseña.Text + "'", conexion);
+            MySqlDataAdapter adaptaDatos = new MySqlDataAdapter(comando);
+            adaptaDatos.Fill(datos);
+            if (datos.Rows.Count == 1 )
+            {
+                this.Hide();
+                new DashboardWindow().Show();
+                
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña incorrectos");
+            }                       
         }
     }
 }
